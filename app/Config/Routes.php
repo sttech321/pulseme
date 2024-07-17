610e4d52c776544b2ed985b91c2a4447678e2dca
sttech321 <st.tech321@gmail.com>
2024-07-17 21:24:16 +0530

    update changes of bio and pulsecheck

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -23,8 +23,10 @@ $routes->add('/forgot-password/verify-otp', 'UserController::verifyOtp');
 $routes->post('/forgot-password/verify-otp/process', 'UserController::verifyOtpProcess');
 $routes->add('/forgot-password/reset', 'UserController::resetPassword');
 $routes->post('/forgot-password/reset/process', 'UserController::resetPasswordProcess');
-$routes->get('/settings/dispatch/campaigns', 'Home::campaigns');
+$routes->get('/settings/dispatch/campaigns', 'Campaign::index');
 $routes->post('/settings/dispatch/campaigns/create', 'Campaign::create');
+$routes->post('/settings/dispatch/campaigns/update/(:num)', 'Campaign::update/$1');
+$routes->get('/settings/dispatch/campaigns/delete/(:num)', 'Campaign::delete/$1');
 // $routes->get('login2', 'Home::dashboard');
 // $routes->get('/forgetPass', 'UserController::forgetPass');
 $routes->get('/settings/dispatch/notifications', 'Home::dispatch_notifications');
@@ -37,12 +39,12 @@ $routes->get('/analyse/overview', 'Home::overview');
 $routes->get('/leaderboard/summary', 'Home::summary');
 $routes->get('/send-referral', 'Home::referral');
 $routes->get('/two-way-messaging', 'Home::messaging');
-// $routes->get('/operate/dispatch', 'Home::dispatch');
 $routes->get('/operate/dispatch', 'TechnicianController::dispatch');
+$routes->get('search', 'TechnicianController::search');
 $routes->get('/chat-widget', 'Home::widget');
 $routes->get('/employee-rewards', 'Home::rewards');
 $routes->get('/analyze/competitor-analysisg', 'Home::analysisg');
-$routes->get('/analyze/reviews', 'Home::reviews');
+// $routes->get('/analyze/reviews', 'Home::reviews');
 // $routes->get('/analyze/dispatching', 'Home::dispatching');
 // $routes->get('/analyze/dispatching', 'TechnicianController::dispatching');
 $routes->get('/leaderboard/reports/departments', 'Home::departments');
@@ -65,18 +67,21 @@ $routes->get('/settings/billing/billing_subscription', 'Home::billing_subscripti
 $routes->get('/show-access-token', 'TechnicianController::showAccessToken');
 $routes->get('/get-response-data', 'TechnicianController::gettechniciandata');
 
-$routes->post('technician/uploadImage', 'TechnicianController::uploadImage');
-$routes->get('search', 'TechnicianController::search');
+// $routes->post('technician/uploadImage', 'TechnicianController::uploadImage');
 // $routes->get('/settings/dispatch/campaigns', 'Home::campaigns');
 $routes->get('settings/general/connect-social-media', 'Home::connect_social_media');
 
 // $routes->get('/welcome', 'Home::welcome');
 
 $routes->get('/dispatch-tab/demo-test', 'DispatchController::insert_campaign');
-// $routes->post('/insert_campaign', 'DispatchController::insert_campaign');
-// $routes->get('/display-tables', 'UserController::displayTableNames');
-$routes->get('/display-tables', 'CampaignController::create');
-$routes->post('/display-tables', 'CampaignController::create');
+$routes->post('/analyze/reviews/create', 'ReviewController::insert');
+$routes->get('/settings/dispatch/campaigns', 'Campaign::index');
+$routes->get('/analyze/reviews', 'Campaign::reviews');
+
+$routes->post('/operate/dispatch/create', 'CustomerController::create');
+
+$routes->get('application/bio/(:num)', 'Campaign::technician_bio/$1');
+$routes->get('application/pulsecheck/(:num)', 'Campaign::pulse_check/$1');
 
 
-
+// $routes->get('/fetch-reviews', 'Review::fetchAndStore');
--- app/Models/TechnicianModal.php
+++ app/Models/TechnicianModal.php
@@ -13,99 +13,13 @@ class technicianModal extends Model
     {
         parent::__construct();
         // Load table name from environment variable
-        $this->table = getenv('TECHNICIAN_TABLE');
+        $this->table = getenv('CAMPAIGN_TABLE');
         // Load allowed fields from environment variable and convert to array
-        $fields = getenv('TECHNICIAN_FIELD');
+        $fields = getenv('CAMPAIGN_FIELD');
         $this->allowedFields = explode(',', $fields);
     }
 
-    public function getAccessToken()
-    {
-        $accessToken = getenv('SUCCESSWARE_ACCESS_TOKEN');
-        return $accessToken;
-    }
-
-    public function get_technician_data()
-    {
-        $accesstoken = getenv('SUCCESSWARE_ACCESS_TOKEN');
-        $Apiurl = getenv('TECHNICIAN_API');
-    
-        $curl = curl_init();
-    
-        $postData = array(
-            "query" => "query SearchTechnicians(\$input: SearchTechnicianInput) {\n  searchTechnicians(input: \$input) {\n    ...PageablePersonnelInfo\n    content {\n      ...PersonnelInfo\n      schedules {\n        ...WorkScheduleInfo\n        __typename\n      }\n      skills {\n        ...WorkSkillInfo\n        __typename\n      }\n      jobClasses {\n        ...JobClassOutputInfo\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment PageablePersonnelInfo on PageablePersonnel {\n  totalElements\n  totalPages\n  pageSize\n  pageNumber\n  numberOfElements\n}\n\nfragment PersonnelInfo on Personnel {\n  id\n  employeeCode\n  fullName\n  firstName\n  lastName\n  email\n  employeeType\n  department\n  apprentice\n  monBeginTime\n  monEndTime\n  tueBeginTime\n  tueEndTime\n  wedBeginTime\n  wedEndTime\n  thuBeginTime\n  thuEndTime\n  friBeginTime\n  friEndTime\n  satBeginTime\n  satEndTime\n  sunBeginTime\n  sunEndTime\n  beginTime\n  endTime\n  isApprentice\n}\n\nfragment WorkScheduleInfo on WorkSchedule {\n  id\n  dayOfWeek\n  startTime\n  endTime\n}\n\nfragment WorkSkillInfo on WorkSkill {\n  id\n  category\n  types\n}\n\nfragment JobClassOutputInfo on JobClassOutput {\n  jobClass\n  jobTypes\n}\n",
-            "operationName" => "SearchTechnicians",
-            "variables" => array(
-                "input" => array(
-                    "capabilities" => array(),
-                    "availabilities" => array(),
-                    "page" => 0,
-                    "size" => 1000,
-                    "sort" => array(),
-                    "firstName" => "",
-                    "lastName" => "",
-                    "names" => array()
-                )
-            )
-        );
-    
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => $Apiurl,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => json_encode($postData),
-            CURLOPT_HTTPHEADER => array(
-                'Content-Type: application/json',
-                'Authorization: Bearer ' . $accesstoken
-            ),
-        ));
-        
-        $response = curl_exec($curl);
-        
-        if (curl_errno($curl)) {
-            $error_msg = curl_error($curl);
-            curl_close($curl);
-            return 'Curl error: ' . $error_msg;
-        }
-        
-        curl_close($curl);
-        
-        $responseData = json_decode($response, true);
-        
-        // return $responseData;
-        if (isset($responseData['data']['searchTechnicians']['content'])) {
-            $technicians = $responseData['data']['searchTechnicians']['content'];
-            foreach ($technicians as $technician) {
-                // Prepare the data to be inserted
-                $data = [
-                    'technician_name' => $technician['fullName'],
-                    'technician_code' => $technician['employeeCode'],
-                    'technician_gmail' => $technician['email'],
-                    'departments' => $technician['department'],
-                    // Add other fields as necessary
-                ];
-                
-                // Insert data into the database
-                $this->insert($data);
-                // print_r($data);
-            }
-        } else {
-            echo 'No technician data found.';
-        }
-
-    }
-    
-    public function uploadImage($technicianId, $imageData)
-    {
-        $imageData = $this->db->escapeString($imageData);
-        $this->update($technicianId, ['technician_image' => $imageData]);
-    }
-
+   
     public function getTechnicianImage($technicianId)
     {
         return $this->where('id', $technicianId)->first();
